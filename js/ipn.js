b72be3e004db6f067eaccf5959b4473ee45a3783
Mees Gelein <meesgelein@gmail.com>
2018-03-14 17:03:36 +0100

    Working on menu

--- js/ipn.js
+++ js/ipn.js
@@ -4,6 +4,8 @@
 $(document).ready(function(){
     //Prevent Ajax caching
     $.ajaxSetup({cache: false});
+    //Run the initialization function of the IPN website
+    ipn.init();
 });
 
 /**
@@ -20,7 +22,20 @@ const ipn = {
     init: function(){
         //Load the menu structure json
         $.get('data/structure.json', function(data){
-            ipn.menu = JSON.parse(data);
+            ipn.menu = data;
+            //Create the nav bar now that the data is loaded
+            ipn.createNavBar();
         });
+    },
+
+    /**
+     * Starts to create the nav bar
+     */
+    createNavBar: function(){
+        //Go through all the menu items
+        var html = "";
+        for(var i = 0; i < ipn.menu.length; i++){
+
+        }
     }
 }
