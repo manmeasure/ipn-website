6239510233fc6d7abfb63b69f57b8834769ef4c9
Mees Gelein <meesgelein@gmail.com>
2018-03-14 16:52:15 +0100

    First attempt at MVC design

--- js/ipn.js
+++ js/ipn.js
@@ -2,5 +2,25 @@
  * Entry point for the JS code. Called when the page is done loading
  */
 $(document).ready(function(){
-    
+    //Prevent Ajax caching
+    $.ajaxSetup({cache: false});
 });
+
+/**
+ * Main global variable to prevent global namespace pollution
+ */
+const ipn = {
+    author: "Mees Gelein",
+    version: "0.0.1",
+    menu: "",
+
+    /**
+     * Starts loading the main menu data
+     */
+    init: function(){
+        //Load the menu structure json
+        $.get('data/structure.json', function(data){
+            ipn.menu = JSON.parse(data);
+        });
+    }
+}
